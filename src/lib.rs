13ef34c9a27dd372f3dd2ea0d37a3c2a8d7ec1da
may <johannes.may@udo.edu>
2020-12-19 22:09:22 +0100

    Removed tests from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,5 @@
 use std::{io::{self, BufRead}, str::FromStr};
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
 pub fn get_input_vec<T: FromStr>() -> Vec<T> {
     let stdin = io::stdin();
     stdin.lock().lines()
